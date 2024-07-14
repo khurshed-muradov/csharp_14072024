4899b4be3db48188f58a274f588b3ee5434015d7
Khurshed Muradov <hurshed@gmail.com>
2024-07-14 20:11:31 +0500

    csharp_seminar:task_0

--- task_0/Program.cs
+++ task_0/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+System.Console.Write("Введите число: ");
+int digit = int.Parse(Console.ReadLine());
+int result = digit * digit;
+System.Console.WriteLine("Квадрат числа: " + digit + " = " + result);
