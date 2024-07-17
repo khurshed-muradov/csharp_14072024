e9b9c8e56519d473154c6a7d669164b188fd34e4
Khurshed Muradov <hurshed@gmail.com>
2024-07-17 21:48:05 +0500

    added csharp:task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+int digitN = int.Parse(Console.ReadLine());
+int digitNnegative = digitN - digitN * 2;
+int i = digitNnegative;
+while (i <= digitN)
+{
+
+    System.Console.WriteLine(i);
+    i = i + 1;
+}
