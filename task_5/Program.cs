349e3bfa9f0d2e0e67c394789a26d47f0ff7d3d0
Khurshed Muradov <hurshed@gmail.com>
2024-07-18 22:41:48 +0500

    added csharp_seminar:task_5

---
+++ task_5/Program.cs
@@ -0,0 +1,23 @@
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+
+int number = new Random().Next(10, 100);
+int tens = number / 10;
+int units = number % 10;
+if (tens > units)
+{
+    System.Console.WriteLine(number + " -> " + tens);
+}
+else
+{
+    System.Console.WriteLine(number + " -> " + units);
+}
+
+
+
+
