1cbe22d5c23fe49e917b6c40e17bfc1e327eb332
Khurshed Muradov <hurshed@gmail.com>
2024-07-20 14:57:38 +0500

    added test folder

---
+++ test/Program.cs
@@ -0,0 +1,10 @@
+﻿Random random = new Random();
+
+int randomNumber1 = random.Next(1, 10);
+System.Console.WriteLine(randomNumber1);
+
+int randomNumber2 = random.Next(10, 100);
+System.Console.WriteLine(randomNumber2);
+
+int randomNumber3 = random.Next(100, 1000);
+System.Console.WriteLine(randomNumber3);
