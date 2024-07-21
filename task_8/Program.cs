71ad21852fc2196dab397641f311eb689efb4b3f
Khurshed Muradov <hurshed@gmail.com>
2024-07-21 16:10:02 +0500

    added task 8 and task 9

--- task_8/Program.cs
+++ task_8/Program.cs
@@ -4,3 +4,23 @@
 // 14 -> нет
 // 46 -> нет
 // 161 -> да
+
+int UserInput()
+{
+    int im_userInput = int.Parse(Console.ReadLine()!);
+    return im_userInput;
+}
+
+int om_userInput = UserInput();
+
+if (om_userInput % 7 == 0 && om_userInput % 23 == 0)
+{
+    System.Console.WriteLine(om_userInput + " -> да");
+}
+else
+{
+    System.Console.WriteLine(om_userInput + " -> нет");
+}
+
+
+
--- task_9/Program.cs
+++ task_9/Program.cs
@@ -5,3 +5,19 @@
 // -4, 16 -> да
 // 25, 5 -> да
 // 8,9 -> нет
+int UserInput()
+{
+    int im_userInput = int.Parse(Console.ReadLine()!);
+    return im_userInput;
+}
+int om_FirstUserInput = UserInput();
+int om_SecondUserInput = UserInput();
+
+if (om_FirstUserInput * om_FirstUserInput == om_SecondUserInput || om_SecondUserInput * om_SecondUserInput == om_FirstUserInput)
+{
+    System.Console.WriteLine(om_FirstUserInput + ", " + om_SecondUserInput + " -> да");
+}
+else
+{
+    System.Console.WriteLine(om_FirstUserInput + ", " + om_SecondUserInput + " -> нет");
+}
