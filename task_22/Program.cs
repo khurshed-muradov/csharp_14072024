f676ab95a67ddc1ce330601d5d5c29bae83caf07
Khurshed Muradov <hurshed@gmail.com>
2024-08-19 17:37:52 +0500

    added task_22 and task_22_2

---
+++ task_22/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+
+int GetSquare(string message)
+{
+    System.Console.Write(message + " ");
+    int im_getSquare = int.Parse(Console.ReadLine()!);
+    return im_getSquare;
+}
+
+int n = GetSquare("Введите число:");
+
+for (int i = 1; i <= n; i++)
+{
+    System.Console.Write((i * i) + " ");
+}
---
+++ task_22_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+
+int GetNumber(string message)
+{
+    System.Console.Write(message + " ");
+    int im_getNumber = int.Parse(Console.ReadLine()!);
+    return im_getNumber;
+}
+
+void GetSquare(int n)
+{
+    int count = 1;
+    while (count <= n)
+    {
+        if (count == n)
+        {
+            System.Console.WriteLine(count * count);
+        }
+        else
+        {
+            System.Console.Write(count * count + " ");
+        }
+        count++;
+    }
+}
+int n = GetNumber("Введите число:");
+GetSquare(n);
