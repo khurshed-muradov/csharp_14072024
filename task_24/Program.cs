e27f1328d1533a3b9a57116863f4fe0b5bab8758
Khurshed Muradov <hurshed@gmail.com>
2024-09-02 07:16:31 +0500

    added tasks 24 26 28

---
+++ task_24/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int GetNumber(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+
+void GetNumberSum(int a)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= a; i++)
+    {
+        sum = sum + i;
+    }
+    System.Console.WriteLine($"Cумма чисел от 1 до {a} = {sum}");
+}
+int A = GetNumber("Введите число: ");
+GetNumberSum(A);
