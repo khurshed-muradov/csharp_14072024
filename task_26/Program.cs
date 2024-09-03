7123c81a545682b9e6bf0d264065cb93edbb412d
Khurshed Muradov <hurshed@gmail.com>
2024-09-03 12:11:43 +0500

    added tasks 26 and 28

--- task_26/Program.cs
+++ task_26/Program.cs
@@ -3,3 +3,21 @@
 // 456 -> 3
 // 78 -> 2
 // 89126 -> 5 
+
+int Call(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int Times(int callNumber)
+{
+    int count = 0;
+    for (count = 0; callNumber > 0; count++)
+    {
+        callNumber = callNumber / 10;
+    }
+    return count;
+}
+int callNumber = Call("Введите число: ");
+int times = Times(callNumber);
+System.Console.WriteLine($"Количество цифр в числе {callNumber} = {times}");
--- task_28/Program.cs
+++ task_28/Program.cs
@@ -3,3 +3,23 @@
 // произведение чисел от 1 до N.
 // 4 -> 24
 // 5 -> 120
+
+int CallNumber(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int Product(int callNumber)
+{
+    int product = 1; // Инициализируется счетчик произведения чисел от 1 до N.
+    for (int i = 1; i <= callNumber; i++)
+    {
+        product = i * product;
+    }
+    return product;
+}
+
+int callNumber = CallNumber("Введите число: ");
+int product = Product(callNumber);
+System.Console.WriteLine(product);
